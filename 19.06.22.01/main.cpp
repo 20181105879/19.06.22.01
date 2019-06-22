7cbe4947448712ba961605bebec61f18460bbf8f
20181105879 <2605753145@qq.com>
2019-06-22 12:15:11 +0800

    Initial Commit

---
+++ 19.06.22.01/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  19.06.22.01
+//
+//  Created by s20181105879 on 2019/6/22.
+//  Copyright © 2019 s20181105879. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
